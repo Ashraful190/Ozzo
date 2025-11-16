74ca078bc4b9935be1d1c7b918956c7f4bb040b1
Ashraful <mdashraful131255@gmail.com>
2025-11-16 23:47:42 +0600

    initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,8 @@ dependencies {
     implementation(libs.firebase.storage)
     implementation(libs.firebase.messaging)
     implementation(libs.firebase.analytics)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/haxio/ozzo/views/starter/MainActivity.kt
+++ app/src/main/java/com/haxio/ozzo/views/starter/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.haxio.ozzo
+package com.haxio.ozzo.views.starter
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.haxio.ozzo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
